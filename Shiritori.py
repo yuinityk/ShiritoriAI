8a2ec938b3e2f42af673cc1bc55fdb2d36728eb7
yuinityk <yuuki.initial.yk@gmail.com>
2016-12-16 13:08:47 +0900

    Fixed Shiritori.py
    get the last letter in input word

--- Shiritori.py
+++ Shiritori.py
@@ -4,6 +4,10 @@ import MeCab
 mecab = MeCab.Tagger('-Ochasen')
 mecab.parse('')
 node = mecab.parseToNode('強連結成分分解')
+'''
 while node:
     print(node.surface, node.feature)
     node = node.next
+'''
+
+print(mecab.parse('すもももももももものうち。'.rstrip('、。')).split('\t')[-5][-1])
