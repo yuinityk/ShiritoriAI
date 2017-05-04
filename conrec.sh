8bb50cdfe1d2ec25a905d930948866622e9fd882
yuinityk <yuuki.initial.yk@gmail.com>
2017-05-04 13:52:01 +0900

    [Add] $BC18lO?2;MQ%7%'%k%9%/%j%W%H$NDI2C(B
    $BO?2;$b$7$F$_$?!%(B

---
+++ conrec.sh
@@ -0,0 +1,25 @@
+echo "input difficulty:e,n,h"
+read dif
+
+if [ "${dif}" = "e" ]; then
+  dff="easy"
+elif [ "${dif}" = "n" ]; then
+  dff="normal"
+elif [ "${dif}" = "h" ]; then
+  dff="hard"
+else
+  echo 'invalid character'
+  exit 1
+fi
+echo $dff
+
+start=`ls ./recorded/$dff/ -1 | wc -l`
+start=`expr $start + 1`
+echo $start
+echo "input last number"
+read  end
+for i in `seq $(( start )) $(( end ))`
+do
+  head -n $i dic_$dff.csv | tail -n 1
+  arecord -f dat -D hw:2,0 -c 1 ./recorded/$dff/$i.wav
+done
