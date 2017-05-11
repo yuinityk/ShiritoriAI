37a6a50446f6e1b54abfc2484a27ef29a94a94b4
yuinityk <yuuki.initial.yk@gmail.com>
2017-05-11 11:53:42 +0900

    [Add] メイちゃん(nitechのマスコットキャラクター)でしゃべるようにした

--- jtalk.py
+++ jtalk.py
@@ -1,11 +1,12 @@
 #-*- coding:utf-8 -*-
+import os
 import subprocess
 from datetime import datetime
 
 def jtalk(t): #tはbytes型
     open_jtalk = ['open_jtalk']
     mech = ['-x', '/var/lib/mecab/dic/open-jtalk/naist-jdic']
-    htsvoice = ['-m', '/usr/share/hts-voice/mei/mei_normal.htsvoice']
+    htsvoice = ['-m', './mei_normal.htsvoice']
     speed = ['-r', '1.0']
     outwav = ['-ow', 'open_jtalk.wav']
     cmd = open_jtalk + mech + htsvoice + speed + outwav
